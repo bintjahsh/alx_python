6f5dfb4e46ec9a982195a6908c998fbba51cb0aa
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-15 12:35:21 +0100

    adds task 6

---
+++ python-functions/6-password.py
@@ -0,0 +1,19 @@
+def validate_password(password):
+    count = 0
+    while count < len(password):
+        if  len(password) >= 8 & count >= 4:
+            for a in password:
+                if a.isupper() == True:
+                        count += 1
+                elif a.islower() == True:
+                        count += 1
+                elif a.isdigit() == True:
+                        count += 1
+                elif a.isspace() == True:
+                        count += 1
+                else:
+                        return False
+        return True
+
+print(validate_password("abc123"))
+

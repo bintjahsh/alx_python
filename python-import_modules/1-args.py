705a9d46ac4af5d80b8b64a9857137c5ac5a941d
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-19 21:27:40 +0100

    adds task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,13 @@
-def count_arg(*args):
-    n = len(args)
-    if n == 1:
-        return f'{n} argument):\n {args}'
-    elif n == 0:
-        return f'{n} arguments.'
-    else:
-        for arg in args[:]:
-            return f'{n} arguments: \n {arg}'
+import sys
 
-print(count_arg(24, 5, "oruko", "jerry"))
-#print(count_arg())
+if __name__ == "__main__":
+    #def count_arg(*argv):
+        n = len(sys.argv)
+        if n == 2:
+            print('{:d} argument:\n{:s}'.format(n, sys.argv[1]))
+        elif n == 1:
+            print('{:d} arguments.'.format(n-1))
+        else:
+                print('{:d} arguments:\n{:s}'.format(*sys.argv[1:], sep='\n'))
+
+#count_arg(24, 5, "oruko", "jerry")

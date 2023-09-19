1f54a93c7c54c3036cae40fc12897fd159699ae5
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-19 21:42:23 +0100

    edits task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     #def count_arg(*argv):
         n = len(sys.argv)
         if n == 1:
-            print('{:d} argument:\n{:s}'.format(n, sys.argv[0:].enumerate()))
+            print('{:d} argument:\n{:s}'.format(n, enumerate(sys.argv[0:])))
         elif n == 0:
             print('{:d} arguments.'.format(n))
         else:
-                print('{:d} arguments:\n{:s}'.format(*sys.argv[0:].enumerate(), sep='\n'))
+                print('{:d} arguments:\n{:s}'.format(enumerate(*sys.argv[0:]), sep='\n'))
 
 #count_arg(24, 5, "oruko", "jerry")

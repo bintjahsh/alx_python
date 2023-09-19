e0765d156738c32928148e7d2f475c2363e4e0e6
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-19 21:29:30 +0100

    adds task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,9 +3,9 @@ import sys
 if __name__ == "__main__":
     #def count_arg(*argv):
         n = len(sys.argv)
-        if n == 2:
+        if n == 1:
             print('{:d} argument:\n{:s}'.format(n, sys.argv[1]))
-        elif n == 1:
+        elif n == 0:
             print('{:d} arguments.'.format(n-1))
         else:
                 print('{:d} arguments:\n{:s}'.format(*sys.argv[1:], sep='\n'))

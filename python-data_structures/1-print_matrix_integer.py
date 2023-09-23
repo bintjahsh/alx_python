d8bc98ab3df13451e8bb2e7f3f9d5c333431d789
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 23:46:04 +0100

    adds task 1

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,21 @@
+def print_matrix_integer(matrix=[[]]):
+    # print(matrix[0][0])
+    # print(matrix[1])
+    # print(matrix[2])
+    for i, j, k in matrix:
+        print(i, j, k)
+        # for j in matrix:
+        #     print(j)
+        #     for k in matrix:
+        #         print(k)
+                # newMatrix = '{:d} {:d} {:d}'.format(i, j, k)
+    # print(newMatrix)
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print_matrix_integer(matrix)
+# print("--")

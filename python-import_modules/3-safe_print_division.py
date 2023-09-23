6f359777277021dc3111484e21bca6d4d3b46d87
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 13:28:17 +0100

    adds task 3,13

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,8 +5,7 @@ def safe_print_division(a, b):
             result = 'None'
             return result
     finally:
-        # print('Inside result: {}'.format(result))
-        return result
+        print('Inside result: {}'.format(result))
 
 # a = 10
 # b = 2

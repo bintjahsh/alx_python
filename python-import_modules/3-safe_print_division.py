961d441d69b27010606a3daec200b1c41d826ee1
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 12:31:50 +0100

    correct task 3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,8 +5,8 @@ def safe_print_division(a, b):
             result = 'None'
             return result
     finally:
-        value = 'Inside result: {}\n{:d} / {:d} = {}'.format(result, a, b, result)
-        return value
+        # value = 'Inside result: {}\n{} / {} = {}'.format(result, a, b, result)
+        return result
 
-#print(safe_print_division(0, 2))
+# print(safe_print_division(0, 2))
 

032d17923a11217244235373f81e643a7ef763ed
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-24 00:13:17 +0100

    adds task 2

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,8 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+    return (length, first)
+
+# sentence = "At Holberton school, I learnt C!"
+# length, first = multiple_returns(sentence)
+# print("Length: {:d} - First character: {}".format(length, first))

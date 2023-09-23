e5a28e81d4b3e73b37ae688afaeec5d1322e9d8f
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 15:14:03 +0100

    adds task 1.12

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,18 @@
 import sys
 
 if __name__ == "__main__":
-    def list_arg(*argv):
-        n = len(argv)
-        if n == 1:
-            print('{:d} argument:'.format(n))
-            for i, arg in enumerate(list(argv), 1):
-                print(('{}: {}'.format(i, arg)))
-        elif n == 0:
-            print('{:d} arguments.'.format(n))
-        else:
-            print('{:d} arguments:'.format(n))
-            for i, arg in enumerate(list(argv), 1):
-                print(('{}: {}'.format(i, arg)))
+# def list_arg(*argv):
+    n = len(argv)
+    if n == 1:
+        print('{:d} argument:'.format(n))
+        for i, arg in enumerate(list(argv), 1):
+            print(('{}: {}'.format(i, arg)))
+    elif n == 0:
+        print('{:d} arguments.'.format(n))
+    else:
+        print('{:d} arguments:'.format(n))
+        for i, arg in enumerate(list(argv), 1):
+            print(('{}: {}'.format(i, arg)))
 
 # list_arg(1, 5, "Hello", "Kwara")
-list_arg()
+# list_arg("Hello")

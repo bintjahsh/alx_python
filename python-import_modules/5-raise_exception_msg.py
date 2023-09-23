d340b6fb5047d3d2bc6b3a2535697279f097f3a1
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 13:20:35 +0100

    adds task 5.3

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,2 +1,8 @@
 def raise_exception_msg(message=""):
-    raise Exception('{:s}'.format(message))
+    raise NameError('{:s}'.format(message))
+
+# try:
+#     message = "C is fun"
+#     raise_exception_msg(message)
+# except NameError as ne:
+#     print(ne)

b8670622cd2e7566f39936711b101d9759670e3a
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 13:27:07 +0100

    adds task 3.12

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,11 +5,11 @@ def safe_print_division(a, b):
             result = 'None'
             return result
     finally:
-        print('Inside result: {}'.format(result))
+        # print('Inside result: {}'.format(result))
         return result
 
-a = 10
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+# a = 10
+# b = 2
+# result = safe_print_division(a, b)
+# print("{:d} / {:d} = {}".format(a, b, result))
 

bc65df422b6f493c03a0716abb7fbbd822bab18d
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 15:11:14 +0100

    adds task 1.11

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,6 @@
 import sys
 
 if __name__ == "__main__":
-        # n = len(sys.argv)
-        # if n == 1:
-        #     print('{:d} argument:\n{}'.format(n, sys.argv[0]))
-        # elif n <= 1:
-        #     print('{:d} arguments.'.format(n))
-        # else:
-        #         print('{:d} arguments:\n{:d}: {}'.format(n, 'i, j in enumerate(*sys.argv[1:])', sep='\n'))
     def list_arg(*argv):
         n = len(argv)
         if n == 1:
@@ -22,4 +15,4 @@ if __name__ == "__main__":
                 print(('{}: {}'.format(i, arg)))
 
 # list_arg(1, 5, "Hello", "Kwara")
-# list_arg("hello")
+list_arg()

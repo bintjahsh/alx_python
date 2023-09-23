17eb7d80afdd0fe0cf840ace53718ca62c21ed2b
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-23 14:35:45 +0100

    adds tyask 1.9

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,18 +8,17 @@ if __name__ == "__main__":
         #     print('{:d} arguments.'.format(n))
         # else:
         #         print('{:d} arguments:\n{:d}: {}'.format(n, 'i, j in enumerate(*sys.argv[1:])', sep='\n'))
-    def list_arg(*args):
-        n = len(args)
-        lst_arg = []
+    def list_arg(*argv):
+        n = len(argv)
         if n == 1:
             print('{:d} argument:'.format(n))
-            for i, arg in enumerate(list(args)):
+            for i, arg in enumerate(list(argv), 1):
                 print(('{}: {}'.format(i, arg)))
         elif n == 0:
             print('{:d} arguments.'.format(n))
         else:
             print('{:d} arguments:'.format(n))
-            for i, arg in enumerate(list(args)):
+            for i, arg in enumerate(list(argv), 1):
                 print(('{}: {}'.format(i, arg)))
 
 # list_arg(1, 5, "Hello", "Kwara")

7021b00305c0e300106e0010b8956a57b225c5f2
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-25 18:09:59 +0100

    adds task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,14 +2,14 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         rowMatrix = ""
         for j in i:
-            rowMatrix += ('{} '. format(j))
-        print(rowMatrix)
+            rowMatrix += ('{} '.format(j))
+        print('{}'. format(rowMatrix))
 
-matrix = [
-    [1, 2, 3],
-    [2, 4, 5],
-    [3, 6, 8]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [2, 4, 5],
+#     [3, 6, 8]
+# ]
 
-print_matrix_integer(matrix)
-print("--")
+# print_matrix_integer(matrix)
+# print("--")

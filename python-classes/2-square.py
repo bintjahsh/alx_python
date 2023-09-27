e5b2dd4332193e8038df0cc30cea262f94441459
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-27 18:00:08 +0100

    tests task 3.1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,6 @@
 """ This module contains the creation and initialization of a class
-called 'Square'. 
+called 'Square'. Square will have the properties: size and area
+- derived from size
 """
 class Square:
     """Creation of class Square which will contain different attributes
@@ -12,29 +13,29 @@ class Square:
         self.__size = size
         self.square_area = size**2
 
-    def size(self):
-        """Retrieve the size of the square as a
-        public attribute
-        """
-        return self.__size
+        def size(self):
+            """Retrieve the size of the square as a
+            public attribute
+            """
+            return self.__size
 
-    def size(self, value):
-        """Set square property 'size' to the value
-        of 'value'
-        """
-        self.size = value
-        
-        """Size must be an integer, otherwise raise
-        a TypeError
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
+        def size(self, value):
+            """Set square property 'size' to the value
+            of 'value'
+            """
+            self.size = value
+            
+            """Size must be an integer, otherwise raise
+            a TypeError
+            """
+            if type(value) is not int:
+                raise TypeError('size must be an integer')
 
-        """Size must be greater than 0, otherwise
-        raise a ValueError
-        """
-        if value < 0:
-            raise ValueError('size must be >= 0')
+            """Size must be greater than 0, otherwise
+            raise a ValueError
+            """
+            if value < 0:
+                raise ValueError('size must be >= 0')
 
     def area(self):
             """Return the current square area

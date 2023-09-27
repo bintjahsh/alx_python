af3958f8e963f8feb85a3d7d7366cc9e1ea5625c
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-27 16:59:22 +0100

    tests task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,28 @@
+""" This module contains the creation and initialization of a class
+called 'Square'. 
+"""
+class Square:
+    """Creation of class Square which will contain different attributes
+    of a square in a private and protected capacity
+    """
+    def __init__(self, size=0):
+        """initializing Square with a private instance attribute 'size'
+        as the size of every square is unique and consequential
+        """
+        self.__size = size
+        
+        """ Size must be an integer, otherwise raise
+        a TypeError
+        """
+        if size is not int:
+            raise TypeError('size must be an integer')
+
+        """ Size must be greater than 0, otherwise
+        raise a ValueError
+        """
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+
+    
+    

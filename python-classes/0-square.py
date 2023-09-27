83e6ec1121a0ca13a77e5566c215220541e0d7b8
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-27 14:16:17 +0100

    adds task 0.3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,8 +5,8 @@ class Square:
     """Creation of class Square which will contain different attributes
     of a square in a private and protected capacity
     """
-    def __init__(self, __size):
+    def __init__(self, size):
         """initializing Square with a private instance attribute 'size'
         as the size of every square is unique and consequential
         """
-        self.size = __size
+        self.__size = size

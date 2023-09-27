3d3c6f880ed19d27c875f975e9031b1c4b3885fb
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-27 18:27:31 +0100

    tests task 3.3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,29 +13,29 @@ class Square:
         self.__size = size
         self.square_area = size**2
 
-        def size(self):
-            """Retrieve the size of the square as a
-            public attribute
-            """
-            return self.__size
+    def size(self):
+        """Retrieve the size of the square as a
+        public attribute
+        """
+        return self.__size
 
-        def size(self, value):
-            """Set square property 'size' to the value
-            of 'value'
-            """
-            self.size = value
-            
-            """Size must be an integer, otherwise raise
-            a TypeError
-            """
-            if type(value) is not int:
-                raise TypeError('size must be an integer')
+    def size(self, value):
+        """Set square property 'size' to the value
+        of 'value'
+        """
+        self.size = value
+        
+        """Size must be an integer, otherwise raise
+        a TypeError
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
 
-            """Size must be greater than 0, otherwise
-            raise a ValueError
-            """
-            if value < 0:
-                raise ValueError('size must be >= 0')
+        """Size must be greater than 0, otherwise
+        raise a ValueError
+        """
+        if value < 0:
+            raise ValueError('size must be >= 0')
 
     def area(self):
             """Return the current square area

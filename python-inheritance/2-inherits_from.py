b1d9e3815cf2d4736304de907e3bdbbae92c4d4e
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-29 11:19:03 +0100

    adds task 2.7

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,14 +6,18 @@ def inherits_from(obj, a_class):
     """ Returns true if obj is an instance of any
     of the subclasses of a_class and false otherwise
     """
-    # return issubclass(type(obj), type(a_class).__bases__)
-    if (type(obj) in a_class.__subclasses__()) | isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class)
+    # if (type(obj) in a_class.__subclasses__())  isinstance(obj, a_class):
+    #     return True
+    # else:
+    #     return False
 
 # a = True
 # print(inherits_from(a, int))
-# a = True
+# a = "string"
 # print(inherits_from(a, object))
+# should return False
 # print(isinstance(True, object))
+# a = 1
+# print(inherits_from(a, int))
+# should return true

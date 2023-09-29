c5f33340a4fce962cdf0c6e5909b08b91657d553
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-29 07:43:02 +0100

    adds task 0.2

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -5,7 +5,7 @@ def is_same_class(obj, a_class):
     """ Returns true if obj is an instance of a_class
     and false otherwise
     """
-    if isinstance(obj, a_class):
+    if type(obj) == type(a_class):
         return True
     else:
         return False

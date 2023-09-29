28f46285df31c687be90abae7173f352dea22f97
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-29 09:29:20 +0100

    adds task 2

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,9 @@
+""" A module that checks if an object
+is an instance of a class that inherited
+(directly or indirectly) from the specified class
+"""
+def inherits_from(obj, a_class):
+    """ Returns true if obj is an instance of any
+    of the subclasses of a_class and false otherwise
+    """
+    return issubclass(obj, a_class)

9a11fa81f1f74980c6c845c676b938b3408ea0c9
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-30 02:52:30 +0100

    adds task 7

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,29 @@
+""" Shape: this module is about a class Rectangle.
+It inherits the attributes of base class BaseGeometry.
+"""
+
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ A class rectangle that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """ Instantiation with private instance properties
+        width and height
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+
+    def __str__(self, width, height):
+        """ A function that overrides __str__ to specify
+        how strings are rendered for class Rectangle"""
+        return ('[Rectangle] {:d}/{:d}'.format(width, height))
+
+    def area(self, width, height):
+        """ A function that calculates and returns the area of
+         the Rectangle"""
+        area = width * height
+        return area

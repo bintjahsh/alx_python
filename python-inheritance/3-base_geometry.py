ca036b429baf92f89e6c14498a5ad6773ac8eaa4
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-30 01:22:04 +0100

    adds task 3.4

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,3 +3,16 @@
 class BaseGeometry:
     """ An empty class BaseGeometry
     """
+#     def __dir__() -> None:
+#         attributes = super().__dir__()
+        
+#         list_to_return = []
+
+#         for attr in attributes:
+#             if attr != "__init_subclass__":
+#                 list_to_return.append(attr)
+
+#         return list_to_return 
+
+# print(dir(BaseGeometry))
+# print(BaseGeometry)

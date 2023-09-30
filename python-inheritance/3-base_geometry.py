3a938b95156a7c00c15c638df8a8767efdbb5e6d
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-09-30 01:35:01 +0100

    adds task 4

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,16 +3,12 @@
 class BaseGeometry:
     """ An empty class BaseGeometry
     """
-#     def __dir__() -> None:
-#         attributes = super().__dir__()
-        
-#         list_to_return = []
+    def _dir_(cls):
 
-#         for attr in attributes:
-#             if attr != "__init_subclass__":
-#                 list_to_return.append(attr)
-
-#         return list_to_return 
+        """This is to exclude the init_subclass attribute 
+        from printing
+        """
+        return [attribute for attribute in super()._dir() if attribute != '__init_subclass_']
 
 # print(dir(BaseGeometry))
 # print(BaseGeometry)
--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,6 +3,14 @@
 class BaseGeometry:
     """ Initializes the class BaseGeometry
     """
+    def _dir_(cls):
+
+        """This is to exclude the init_subclass attribute 
+        from printing
+        """
+        return [attribute for attribute in super()._dir() if attribute != '__init_subclass_']
+
+
     def area(self):
         """ A function area that raises a custom
         exception when called

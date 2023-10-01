068d02328abb993ab5341f657dc6bf4176d5d7be
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-02 00:51:04 +0100

    adds task 1.2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,12 @@
 """ A class Rectangle that inherits from Base 
 """
-from models.base import Base
+from base import Base
 
 class Rectangle(Base):
     """ A class Rectangle that inherits from super class Base
     """
+    Base.__nb_objects = 0
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initialize Rectangle with private instance attributes
         width, height, x and y
@@ -12,8 +14,19 @@ class Rectangle(Base):
         self.__width = width
         self.__height = height
         self.__x = x
-        self.y = y
-        self.id = Base.id
+        self.__y = y
+        self.id = Base.__nb_objects
+    
+        if id != None:
+            """If a value is given for id assign it to id, otherwise
+            increment __nb_objects and assign its value to id
+            """
+            self.id = id
+            Base.__nb_objects += id
+        else:
+            Base.__nb_objects += 1
+        
+            self.id = Base.__nb_objects
     
     @property
     def width(self):
@@ -62,3 +75,30 @@ class Rectangle(Base):
         """ Defines public setter for attribute width
         """
         self.y = y
+
+# r = Rectangle(12, 14)
+# if r is None:
+#     print("Can't create Rectangle")
+#     exit(1)
+
+# if r._Rectangle__width != 12:
+#     print("Wrong width: {}".format(r._Rectangle__width))
+#     exit(1)
+
+# if r._Rectangle__height != 14:
+#     print("Wrong height: {}".format(r._Rectangle__height))
+#     exit(1)
+
+# if r._Rectangle__x != 0:
+#     print("Wrong x: {}".format(r._Rectangle__x))
+#     exit(1)
+
+# if r._Rectangle__y != 0:
+#     print("Wrong y: {}".format(r._Rectangle__y))
+#     exit(1)
+
+# if r.id != 1:
+#     print("ID is not initialized at 1")
+#     exit(1)
+
+# print("OK", end="")

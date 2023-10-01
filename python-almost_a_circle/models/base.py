6d364b467905f866e0a76c038dba7847eb201221
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-02 00:02:44 +0100

    adds task 0.9

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,23 +7,26 @@ class Base:
     """ A class Base which will serve as the super class
     for all other classes in the AirBnB project
     """
+    __nb_objects = 0
+    """ Set default value of private attribute __nb_objects to 0.
+    """
+
     def __init__(self, id=None):
         """ Initializes the class Base with a keyword argument
-        id. Set default value of __nb_objects to 0. 
+        id.  
         """
-        __nb_objects = 0
 
-        """If a value is given for id assign it to id, otherwise
-        increment __nb_objects and assign its value to id
-        """
-        if id is not None:
+        if id != None:
+            """If a value is given for id assign it to id, otherwise
+            increment __nb_objects and assign its value to id
+            """
             self.id = id
+            Base.__nb_objects += id
         else:
-            __nb_objects += 1
-            self.id = __nb_objects
-            
+            Base.__nb_objects += 1
         
-
+            self.id = Base.__nb_objects
+            
 # b = Base()
 # if b is None:
 #     print("Can't create Base")

7cfd4a2b18ce7169ab111c51c9f6b9e9fafea5ad
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-02 11:32:45 +0100

    adds task 1.9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,104 +1,108 @@
 """ A class Rectangle that inherits from Base 
 """
-from models.base import Base
+from base import Base
 
 class Rectangle(Base):
     """ A class Rectangle that inherits from super class Base
     """
-    Base.__nb_objects = 0
+    # __nb_objects = 0
+    """ Set default value of private attribute __nb_objects to 0.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initialize Rectangle with private instance attributes
         width, height, x and y
         """
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        self.id = Base.__nb_objects
+        # self.id = Base(id) 
+        # self.id = Rectangle.__nb_objects
     
-        if id != None:
-            """If a value is given for id assign it to id, otherwise
-            increment __nb_objects and assign its value to id
-            """
-            self.id = id
-            Base.__nb_objects += id
-        else:
-            Base.__nb_objects += 1
+        # if id != None:
+        #     """If a value is given for id assign it to id, otherwise
+        #     increment __nb_objects and assign its value to id
+        #     """
+        #     self.id = id
+        #     Rectangle.__nb_objects += id
+        # else:
+        #     Rectangle.__nb_objects += 1
         
-            self.id = Base.__nb_objects
+        #     self.id = Rectangle.__nb_objects
     
     @property
     def width(self):
         """ Defines public getter for attribute width
         """
-        self.width = self.__width
+        # self.width = self.__width
+        return self.__width
 
     @width.setter
     def width(self, width):
         """ Defines public setter for attribute width
         """
-        self.width = width
+        self.__width = width
 
     @property
     def height(self):
         """ Defines public getter for attribute width
         """
-        self.height = self.__height
+        return self.__height
 
     @height.setter
     def height(self, height):
         """ Defines public setter for attribute width
         """
-        self.height = height
+        self.__height = height
     
     @property
     def x(self):
         """ Defines public getter for attribute width
         """
-        self.x = self.__x
+        return self.__x
 
     @x.setter
     def height(self, x):
         """ Defines public setter for attribute width
         """
-        self.x = x
+        self.__x = x
 
     @property
     def y(self):
         """ Defines public getter for attribute width
         """
-        self.y = self.__y
+        return self.__y
 
     @y.setter
     def height(self, y):
         """ Defines public setter for attribute width
         """
-        self.y = y
+        self.__y = y
 
-# r = Rectangle(12, 14)
+
+# r = Rectangle(12, 14, 4, 5, 10)
 # if r is None:
 #     print("Can't create Rectangle")
 #     exit(1)
 
 # if r._Rectangle__width != 12:
-#     print("Wrong width: {}".format(r._Rectangle__width))
+#     print("Wrong private width: {}".format(r._Rectangle__width))
 #     exit(1)
 
-# if r._Rectangle__height != 14:
-#     print("Wrong height: {}".format(r._Rectangle__height))
+# if r.width != 12:
+#     print("Wrong width getter: {}".format(r._Rectangle__width))
 #     exit(1)
 
-# if r._Rectangle__x != 0:
-#     print("Wrong x: {}".format(r._Rectangle__x))
+# r.width = 5
+
+# if r._Rectangle__width != 5:
+#     print("Wrong private width: {}".format(r._Rectangle__width))
 #     exit(1)
 
-# if r._Rectangle__y != 0:
-#     print("Wrong y: {}".format(r._Rectangle__y))
-#     exit(1)
-
-# if r.id != 1:
-#     print("ID is not initialized at 1")
+# if r.width != 5:
+#     print("Wrong width getter: {}".format(r._Rectangle__width))
 #     exit(1)
 
 # print("OK", end="")

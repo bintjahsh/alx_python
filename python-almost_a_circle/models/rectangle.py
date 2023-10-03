9afea3d802c68773d0e3e20ebd98469fafc83802
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-03 03:00:33 +0100

    adds task 2.13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,8 @@ class Rectangle(Base):
         """
         if type(width) != int:
             raise TypeError('width must be an integer')
+        elif width <= 0:
+            raise ValueError('width must be > 0')
         else:
             self.__width = width
         
@@ -28,6 +30,8 @@ class Rectangle(Base):
         """
         if type(height) != int:
             raise TypeError('height must be an integer')
+        elif height <= 0:
+            raise ValueError('height must be > 0')
         else:
             self.__height = height
 
@@ -37,6 +41,8 @@ class Rectangle(Base):
         """
         if type(x) != int:
             raise TypeError('x must be an integer')
+        elif x < 0:
+            raise ValueError('x must be >= 0')
         else:
             self.__x = x
         
@@ -46,6 +52,8 @@ class Rectangle(Base):
         """
         if type(y) != int:
             raise TypeError('y must be an integer')
+        elif y < 0:
+            raise ValueError('y must be >= 0')
         else:
             self.__y = y
         

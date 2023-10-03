68161feeef2b615aa1c850e97f4484dc646a49bc
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-03 01:53:18 +0100

    adds task 1.20

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 """ A class Rectangle that inherits from Base 
 """
-from models.base import Base
+from base import Base
 
 class Rectangle(Base):
     """ A class Rectangle that inherits from super class Base
@@ -9,7 +9,7 @@ class Rectangle(Base):
     """ Set default value of private attribute __nb_objects to 0.
     """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width=0, height=0, x=0, y=0, id=None):
         """ Initialize Rectangle with private instance attributes
         width, height, x and y
         """
@@ -18,8 +18,6 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-        # self.id = Base(id) 
-        # self.id = Rectangle.__nb_objects
     
         # if id != None:
         #     """If a value is given for id assign it to id, otherwise
@@ -36,7 +34,6 @@ class Rectangle(Base):
     def width(self):
         """ Defines public getter for attribute width
         """
-        # self.width = self.__width
         return self.__width
 
     @width.setter
@@ -47,37 +44,37 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """ Defines public getter for attribute width
+        """ Defines getter for public attribute height
         """
         return self.__height
 
     @height.setter
     def height(self, height):
-        """ Defines public setter for attribute width
+        """ Defines setter for public attribute height
         """
         self.__height = height
     
     @property
     def x(self):
-        """ Defines public getter for attribute width
+        """ Defines public getter for attribute x
         """
         return self.__x
 
     @x.setter
-    def height(self, x):
-        """ Defines public setter for attribute width
+    def x(self, x):
+        """ Defines public setter for attribute x
         """
         self.__x = x
 
     @property
     def y(self):
-        """ Defines public getter for attribute width
+        """ Defines public getter for attribute y
         """
         return self.__y
 
     @y.setter
-    def height(self, y):
-        """ Defines public setter for attribute width
+    def y(self, y):
+        """ Defines public setter for attribute y
         """
         self.__y = y
 
@@ -87,22 +84,22 @@ class Rectangle(Base):
 #     print("Can't create Rectangle")
 #     exit(1)
 
-# if r._Rectangle__width != 12:
-#     print("Wrong private width: {}".format(r._Rectangle__width))
+# if r._Rectangle__height != 14:
+#     print("Wrong private height: {}".format(r._Rectangle__height))
 #     exit(1)
 
-# if r.width != 12:
-#     print("Wrong width getter: {}".format(r._Rectangle__width))
+# if r.height != 14:
+#     print("Wrong height getter: {}".format(r.height))
 #     exit(1)
 
-# r.width = 5
+# r.height = 7
 
-# if r._Rectangle__width != 5:
-#     print("Wrong private width: {}".format(r._Rectangle__width))
+# if r._Rectangle__height != 7:
+#     print("Wrong private height: {}".format(r._Rectangle__height))
 #     exit(1)
 
-# if r.width != 5:
-#     print("Wrong width getter: {}".format(r._Rectangle__width))
+# if r.height != 7:
+#     print("Wrong height getter: {}".format(r._Rectangle__height))
 #     exit(1)
 
 # print("OK", end="")

767d153b5521ed4bda4cb1afabd54e11ac8ebf0c
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-06 07:59:53 +0100

    adds task 1.19

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,11 +5,12 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    def get_value():
+    def get_value(url):
         """  takes in a URL, sends a request to the URL and 
         displays the value of the variable X-Request-Id in
         the response header
         """
-        url = sys.argv
-        r = requests.get(url)
+        # url = sys.argv
+        headers = {'X-Request-Id': 'None'}
+        r = requests.get(url, headers=headers)
         print('{:s}'.format(r.headers.get('X-Request-Id')))

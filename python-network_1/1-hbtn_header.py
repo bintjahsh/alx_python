803c820b80124add3c271cae938f5e65a24aaf2e
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-06 07:28:59 +0100

    adds task 1.9

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         """
         url = sys.argv[1:-1]
         r = requests.get(url)
-        return r.headers.items['X-Request-Id']
+        return r.headers.get('X-Request-Id')

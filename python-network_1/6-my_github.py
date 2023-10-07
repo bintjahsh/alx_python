45c65019796c4801aecb098ff079f99f47e6d628
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-07 12:39:18 +0100

    adds task 6.3

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -6,10 +6,10 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/user'
     username = sys.argv[1]
-    password = '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'
-    header_dict = {'Authorization': 'Bearer {}'.format(password), 'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
+    password = sys.argv[2]
+    url = 'https://api.github.com/users/{}'.format(username)
+    header_dict = {'Authorization': password, 'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
     
     r = requests.get(url, headers=header_dict)
     res = r.json()

b300bed55b38b780ca9628dc4a4b8cf58ca9fdea
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-07 08:55:51 +0100

    adds task 5

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,25 @@
+""" This module is about a python script using inbuilt module
+requests to takes in a letter and sends a POST request to 
+a url with the letter as a parameter. 
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+        url = 'http://0.0.0.0:5000/search_user'
+
+        if len(sys.argv) == 1:
+            q = ""
+        else:
+            q = sys.argv[1]
+
+        r = requests.post(url, {'q': q})
+        
+        if len(r.text) != 0 & type(r.text) == dict:
+            print('[{}] {}'.format(r.text['id'], r.text['name']))
+        elif len(r.text) == 0:
+            print('No result')
+        elif type(r.text) == dict:
+            print('Not a valid JSON')
+
+        

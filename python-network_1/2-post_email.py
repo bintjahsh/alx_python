346fe82474965f38c51632400bcb4fad0777811c
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-07 07:06:30 +0100

    adds task 2.24

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,6 +7,6 @@ import requests
 if __name__ == "__main__":
         url = sys.argv[0]
         email = sys.argv[1]
-        r = requests.post(url, param=email)
+        payload = email
+        r = requests.post(url, data=payload)
         print(r.text)
-

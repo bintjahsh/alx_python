a00a95c82b110f09789ca70e243c60752592d457
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-11 11:48:30 +0100

    adds task 3.1

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{:s}' COLLATE utf8mb4_0900_as_cs ORDER BY states.id ASC"
+    query = "SELECT * FROM states WHERE name = '%s' COLLATE utf8mb4_0900_as_cs ORDER BY states.id ASC"
     cursor.execute(query, (state_search,))
     states = cursor.fetchall()
 

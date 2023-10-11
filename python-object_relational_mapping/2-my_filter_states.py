4037d2a26b80b4d20b57437f6adb204ff079ec3c
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-11 08:59:57 +0100

    adds task 2.1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=My_User , passwd=My_Pass , db=My_DB)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_search))
+    cursor.execute("SELECT * FROM states WHERE name = '{}' COLLATE utf8mb4_0900_as_cs ORDER BY states.id ASC".format(state_search))
     states = cursor.fetchall()
     
     for id, state in states:

f95f5c4e6d157290752f157228f5cd153f1074fb
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-11 08:58:27 +0100

    adds task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+""" This module creates a script that takes in an
+argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    My_User = sys.argv[1]
+    My_Pass = sys.argv[2]
+    My_DB = sys.argv[3]
+    state_search = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", user=My_User , passwd=My_Pass , db=My_DB)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_search))
+    states = cursor.fetchall()
+    
+    for id, state in states:
+        print("({:d}, '{:s}')".format(id, state))

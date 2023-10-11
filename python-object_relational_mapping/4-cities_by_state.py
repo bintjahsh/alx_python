87cc700c8266051db1931c27267926590e41cea9
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-11 12:24:52 +0100

    adds task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+""" This module creates a script that lists
+all cities from the database hbtn_0e_4_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    My_User = sys.argv[1]
+    My_Pass = sys.argv[2]
+    My_DB = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=My_User , passwd=My_Pass , db=My_DB)
+
+    cursor = db.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON state_id ORDER BY cities.id ASC"
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for id, state in states:
+        print("({:d}, '{:s}')".format(id, state))

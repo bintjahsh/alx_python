470ea05a54336d6fe5d869bb80c975b26bbda917
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-13 00:13:21 +0100

    adds task 6.24

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,7 @@
 from sqlalchemy import Column, Integer, String
-class State(Base):
+class State():
     """Creation of class State that inherits from Base and links to
     the MySQL table states. State has the following attributes:
-        + attribute id that represents a column of an auto-generated,
-        unique integer, not null and is a primary key
-        + attribute name that represents a column of a string with
-        maximum 128 characters and is not null
     """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, autoincrement=True, primary_key=True)

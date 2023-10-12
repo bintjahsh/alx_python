fc73820b516fafa17f88318ef7492a471fc8711b
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-13 00:42:23 +0100

    adds task 7.3

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,6 +10,6 @@ from model_state import Base, State
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
 Session = sessionmaker(bind=engine)
 
-query = Session.query(State.id, State.name).order_by(State.id):
+query = Session.query(State.id, State.name).order_by(State.id)
 print(query)
 # print("{:d}: {:s}".format(State.id, State.name))

e809fbd0c4a543e4ccd495f6fdee4325f7fd7c31
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-12 07:58:12 +0100

    adds task 6.5

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,7 +17,11 @@ if __name__ == "__main__":
     """
     class State(Base):
         """Creation of class State that inherits from Base and links to
-        the MySQL table states
+        the MySQL table states. State has the following attributes:
+            + attribute id that represents a column of an auto-generated,
+            unique integer, not null and is a primary key
+            + attribute name that represents a column of a string with
+            maximum 128 characters and not null
         """
         __tablename__ = 'states'
         id = Column(Integer, unique=True, nullable=False, autoincrement=True, primary_key=True)

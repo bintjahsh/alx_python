37c4b9bceaf3f82895ced959a7a9c20349db72eb
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-12 07:35:41 +0100

    adds task 6.2

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,7 +6,6 @@ import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Connecting to MySQL server at 23.92.23.113 using mysql-python DBAPI 
 if __name__ == "__main__":
     Base = declarative_base()
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),)
@@ -15,13 +14,14 @@ if __name__ == "__main__":
     
     engine.connect()
     
-    """A class state that inherits from Base and links to
+    """A class State that inherits from Base and links to
         the MySQL table states
     """
     class State(Base):
-        """A class state that inherits from Base and links to
+        """Creation of class State that inherits from Base and links to
         the MySQL table states
         """
+
         __tablename__ = 'states'
         id = Column(Integer, unique=True, nullable=False, autoincrement=True, primary_key=True)
         name = Column(String(128), nullable=False)

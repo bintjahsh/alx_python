322e490c1e10bfd52f345ef850a6a525b8130460
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-13 05:30:07 +0100

    adds task 1

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,19 @@
+""" This module creates a script that starts a Flask
+web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb')
+def second_hello():
+    return ("HBNB")
+
+if __name__ == '__main__':
+    app.run()

dfd5ebf09ac2a23272cdc842d796e72cc974a384
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-13 05:39:18 +0100

    adds task 2

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,23 @@
+""" This module creates a script that starts a Flask
+web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb')
+def second_hello():
+    return ("HBNB")
+
+@app.route('/c/<text>')
+def show_c(text):
+    return ("C {}".format(text))
+
+if __name__ == '__main__':
+    app.run()

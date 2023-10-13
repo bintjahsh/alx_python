a35ef07293b6b17d6c24beb5b86091927023fb47
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-13 06:06:01 +0100

    adds task 3

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,27 @@
+""" This module creates a script that starts a Flask
+web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb')
+def second_hello():
+    return ("HBNB")
+
+@app.route('/c/<text>')
+def show_c(text):
+    return ("C {}".format(text).replace('_', ' '))
+
+@app.route('/python/<text>')
+def show_python(text="is cool"):
+    return ("Python {}".format(text).replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run()

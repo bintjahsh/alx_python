151dc50297ef3ef8babeb306fb2e10580d7f707b
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-14 23:41:23 +0100

    adds task 4.9

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -9,27 +9,27 @@ from flask import Flask
 web application
 """
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def hello():
     return ("Hello HBNB!")
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def second_hello():
     return ("HBNB")
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def show_c(text):
     return ("C {}".format(text).replace('_', ' '))
 
 @app.route('/python/', defaults={'text': 'is_cool'})
 
-@app.route('/python/<text>')
+@app.route('/python/<text>', strict_slashes=False)
 def show_python(text):
     return ("Python {}".format(text).replace('_', ' '))
 
-@app.route('/number/<int:n>')
+@app.route('/number/<int:n>', strict_slashes=False)
 def show_python(n):
     return ("{} is a number".format(n))
 

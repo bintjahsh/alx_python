8e56654a3384a43cee73a80fb6ae8a6ede0eb0d7
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-11-06 18:52:29 +0000

    adds task 2.9

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,40 +2,38 @@ import json
 import requests
 import sys
 
-def getEmployeeTodo(id):
-    url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-    res = requests.get(url)
-    todos = res.json()
-
-    url = f"https://jsonplaceholder.typicode.com/users/{id}"
-    res = requests.get(url)
-    employee = res.json()
-
-    completed_tasks = len([todo for todo in todos if todo['completed']])
-    all_tasks = len(todos)
-
-    # Print progress report
-    print(f"Employee {employee['name']} is done with tasks({completed_tasks}/{all_tasks}):")
-    for todo in todos:
-        if todo['completed']:
-            print(f'\t{todo["title"]}')
-
-    data = {str(id): []}
-    for todo in todos:
-        data[str(id)].append({
-            'task': todo['title'],
-            'completed': todo['completed'],
-            'username': employee['name']
-        })
-
-    with open(f'{id}.json', 'w') as jsonfile:
-        json.dump(data, jsonfile)
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print(f"what is the id: {sys.argv[0]} EMPLOYEE_ID")
         sys.exit(1)
+    def getEmployeeTodo(id):
+        url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+        res = requests.get(url)
+        todos = res.json()
 
-    id = sys.argv[1]
-    getEmployeeTodo(id)
+        url = f"https://jsonplaceholder.typicode.com/users/{id}"
+        res = requests.get(url)
+        employee = res.json()
+
+        completed_tasks = len([todo for todo in todos if todo['completed']])
+        all_tasks = len(todos)
+
+        # Print progress report
+        print(f"Employee {employee['name']} is done with tasks({completed_tasks}/{all_tasks}):")
+        for todo in todos:
+            if todo['completed']:
+                print(f'\t{todo["title"]}')
+
+        data = {str(id): []}
+        for todo in todos:
+            data[str(id)].append({
+                'task': todo['title'],
+                'completed': todo['completed'],
+                'username': employee['name']
+            })
+
+        with open(f'{id}.json', 'w') as jsonfile:
+            json.dump(data, jsonfile)
+
+        id = sys.argv[1]
+        getEmployeeTodo(id)
